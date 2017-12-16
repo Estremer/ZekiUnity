550569869aab63949b76004ff5da962ea7ef072e
Estremer <estremer90@yahoo.com>
2017-12-16 21:05:43 +0100

    Push 1

---
+++ Zenqui/Assets/Script/Menu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour {
+
+    public string nombreEscena;
+
+	// Use this for initialization
+	public void Nuevo () {
+
+	}
+	
+	// Update is called once per frame
+	public void Salir () {
+        Application.Quit();
+	}
+}
